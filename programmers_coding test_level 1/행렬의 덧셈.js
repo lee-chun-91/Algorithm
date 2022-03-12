9cd8fd9460eae42816ab3b4066e58e034f8d5eb4
lee chun <lcg5320@gmail.com>
2022-03-12 14:30:20 +0900

    행렬의 덧셈

---
+++ programmers_coding test_level 1/행렬의 덧셈.js
@@ -0,0 +1,11 @@
+function solution(arr1, arr2) {
+    var answer = [[]];
+    for (var i=0; i<arr1.lenght; i++){
+        answer[i] = [];
+        for(var j=0; j<arr1[i].length; j++){
+            answer[i].push(arr1[i][j]+arr2[i][j]);
+        }
+    }
+    answer[i].push(arr1[i][j]+arr2[i][j]);
+    return answer;
+}
